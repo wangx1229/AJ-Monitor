93c86beb8cc9820c3d3c7ae28b38452d2a5e3a4c
_Songs <wangx1229@outlook.com>
2019-05-05 10:12:28 +0800

    add new method

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 类的继承
 class Monitor {
   constructor(time) {
     const params = {
@@ -65,3 +66,80 @@ class Monitor {
 
 const monitor = new Monitor(1000)
 monitor.init()
+
+// 对象委托
+
+const Monitor = {
+  data: {
+    params: null,
+    nikeshoes: null,
+    url: null
+  },
+  start () {
+    this.data.url = new URL(this.data.url)
+    Object.keys(this.data.params).forEach(key => {
+      if (!Array.isArray(this.data.params[key])) {
+        this.data.url.searchParams.append(key, this.data.params[key])
+      } else {
+        const tempA = this.data.params[key]
+        tempA.forEach(item => this.data.url.searchParams.append(key, item))
+      }
+    })
+    this.getList()
+  },
+  async getList () {
+    const { objects } = await fetch(this.data.url).then(response => {
+      return response.json()
+    }).catch(e => {
+      console.log(e)
+    })
+    if (objects) {
+      this.data.nikeshoes = localStorage.getItem('nikeshoes') ? JSON.parse(localStorage.getItem('nikeshoes')) : {}
+      objects.forEach(item => {
+        if (!this.data.nikeshoes[item.id]) {
+          this.data.nikeshoes[item.id] = true
+          window.open(item.publishedContent.properties.coverCard.properties.portraitURL)
+        }
+      })
+      localStorage.setItem('nikeshoes', JSON.stringify(this.data.nikeshoes))
+    }
+    this.getList()
+  }
+}
+
+const nike = Object.create(Monitor, {
+  data: {
+    value: {
+      params: {
+        anchor: 0,
+        count: 10,
+        filter: [
+          'marketplace(CN)',
+          'language(zh-Hans)',
+          'channelId(010794e5-35fe-4e32-aaff-cd2c74f89d61)',
+          'exclusiveAccess(true,false)'
+        ],
+        fields: [
+          'active',
+          'id',
+          'lastFetchTime',
+          'productInfo',
+          'publishedContent.nodes',
+          'publishedContent.properties.coverCard',
+          'publishedContent.properties.productCard',
+          'publishedContent.properties.products',
+          'publishedContent.properties.publish.collections',
+          'publishedContent.properties.relatedThreads',
+          'publishedContent.properties.seo',
+          'publishedContent.properties.threadType',
+          'publishedContent.properties.custom'
+        ]
+      },
+      nikeshoes: null,
+      url: `https://api.nike.com/product_feed/threads/v2`
+    }
+  }
+})
+
+nike.start()
+
